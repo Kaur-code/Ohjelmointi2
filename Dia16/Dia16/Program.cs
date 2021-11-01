34a1a301b990cf07efde4bd1e4522f2cf664ffcf
unknown <kaur.sarv@edu.keuda.fi>
2021-11-01 10:48:11 +0200

    dia16 tehty

---
+++ Dia16/Dia16/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Dia16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tehtävä 1");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("x:{0} y:{1}", x + 3, y + 3);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 2");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x:{0} y:{1}", x2 - 2, y2 - 2);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 3");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x:{0} y:{1}", x3 * 5, y3 * 5);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 4");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x4 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y4 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x/y = {0}", x4/y4);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 5");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x5 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y5 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x%y = {0}", x5 % y5);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 3");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x6 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y6 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x + y = {0}", x6 + y6);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 3");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x7 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y7 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x - y = {0}", x7 - y7);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 3");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x8 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y8 = double.Parse(Console.ReadLine());
+            double pap = y8 * 5;
+            Console.WriteLine("x * y*5 = {0}", x8 * pap);
+            Console.WriteLine();
+
+            Console.WriteLine("Tehtävä 3");
+            Console.WriteLine("ANNA X:n ARVO");
+            double x9 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ANNA Y:n ARVO");
+            double y9 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x / y = {0}", x9 / y9);
+            Console.WriteLine();
+        }
+    }
+}
