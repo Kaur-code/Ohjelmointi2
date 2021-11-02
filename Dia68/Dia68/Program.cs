4e07f9d1c9a41716ae796034e26ecd882f56174d
unknown <kaur.sarv@edu.keuda.fi>
2021-11-02 11:34:39 +0200

    dia69 tehtäviä

--- Dia68/Dia68/Program.cs
+++ Dia68/Dia68/Program.cs
@@ -13,7 +13,10 @@ namespace Dia68
             Console.WriteLine("Tehtävä 3: peruslaskut.");
             Console.WriteLine("Tehtävä 4: jakojäännös");
             Console.WriteLine("Tehtävä 5: hello!");
-            Console.WriteLine("Tehtävä 6: kertotaulu");
+            Console.WriteLine("Tehtävä 10: kertotaulu");
+            Console.WriteLine("Tehtävä 11: ikäsi");
+            Console.WriteLine("Tehtävä 12: lukuloop");
+            Console.WriteLine("Tehtävä 13: nimen vaihto");
 
             int teht = int.Parse(Console.ReadLine());
             switch (teht)
@@ -33,11 +36,20 @@ namespace Dia68
                 case 5:
                     Hello();
                     break;
-                case 6:
+                case 10:
                     Kertotaulu();
                     break;
+                case 11:
+                    Ika();
+                    break;
+                case 12:
+                    LukuLoop();
+                    break;
+                case 13:
+                    NimenVaihto();
+                    break;
                 default:
-                    Console.WriteLine("Minkä tehtävän haluat minun tehdä? Anna 1-6 numero.");
+                    Console.WriteLine("Minkä tehtävän haluat minun tehdä? Anna tehtävän numero.");
                     goto Alku;
             }
 
@@ -95,6 +107,32 @@ namespace Dia68
                 {
                     Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
                 }
+            }
+
+            static void Ika()
+            {
+                Console.WriteLine("Anna ikäsi: ");
+                string ika = Console.ReadLine();
+                Console.WriteLine("{0} vuotta vanha? Näytät ikäistäsi vanhemmalta.", ika);
+            }
+
+            static void LukuLoop()
+            {
+                Console.WriteLine("Anna lukulooppisi: ");
+                int luku = int.Parse(Console.ReadLine());
+
+                for(int i = 0; i < luku; i++)
+                {
+                    Console.Write(luku);
+                }
+            }
+
+            static void NimenVaihto()
+            {
+                Console.WriteLine("Anna nimesi, jotta voin korjata sen: ");
+                string nimi = Console.ReadLine();
+                char[] arr = nimi.ToCharArray();
+                string parempiNimi = nimi;
             }
             goto Alku;
         }
