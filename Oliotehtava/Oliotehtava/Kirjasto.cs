999974eeea4781b2a630e927a39aad93552821fa
Kaur-code <79134890+Kaur-code@users.noreply.github.com>
2021-11-08 18:13:45 +0200

    Add files via upload

---
+++ Oliotehtava/Oliotehtava/Kirjasto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oliotehtava
+{
+    class Kirjasto
+    {
+        string kirja;
+        int sivut;
+        string kirjailia;
+        string genre2;
+        int kirjamerkkini;
+        public Kirjasto(string kirjaNimi, int sivuMaara, string author, string genre)
+        {
+            kirja = kirjaNimi;
+            sivut = sivuMaara;
+            kirjailia = author;
+            genre2 = genre;
+        }
+
+        public void KirjaMerkki()
+        {
+            Console.WriteLine("Laitat kirjamerkkisi sivuun: ");
+            Alku:
+            kirjamerkkini = int.Parse(Console.ReadLine());
+
+            if(kirjamerkkini > sivut || kirjamerkkini < 1)
+            {
+                Console.WriteLine("Kirjassasi ei ole riittävästi sivuja kirjanmerkin sijaintiin.");
+                Console.WriteLine("Yritä uudelleen.");
+                goto Alku;
+            }
+            else
+            {
+                Console.WriteLine("Kirjamerkki asetettu sivulle {0}", kirjamerkkini);
+            }
+        }
+
+        public void SivuaViela()
+        {
+            Console.WriteLine("Sinulla on {0} sivua jäljellä luettavana kirjasta '{1}'.", sivut - kirjamerkkini, kirja);
+        }
+
+        public void KirjaInfo()
+        {
+            Console.WriteLine("Kirjan nimi on '{0}'. Sen kirjailia: '{1}'. Kirjan genre on {2}. Kirjan sivumäärä: {3}", kirja, kirjailia, genre2, sivut);
+        }
+    }
+}
---
+++ Oliotehtava/Oliotehtava/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oliotehtava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kirjasto paavo = new Kirjasto("Paavo", 92, "Lemmi Kauhanen", "kauhu");
+            paavo.KirjaInfo(); // tämä metodi antaa perustietoja kirjasta
+            paavo.KirjaMerkki(); // tämä metodi asettaa kirjamerkin jollekin kirjan sivulle
+            paavo.SivuaViela(); // tämä metodi laskee kuinka paljon sivua on vielä luettavana. Käyttää kirjamerkkiä laskussaan.
+
+            Kirjasto lottovoitto = new Kirjasto("Lottovoitto", 261, "Viisi Sumo", "fantasia");
+            lottovoitto.KirjaInfo();
+            lottovoitto.KirjaMerkki();
+            lottovoitto.SivuaViela();
+
+            Kirjasto captainAlex = new Kirjasto("Who Killed Captain Alex", 1058, "VJ Emmie", "ACTION");
+            captainAlex.KirjaInfo();
+            captainAlex.KirjaMerkki();
+            captainAlex.SivuaViela();
+        }
+    }
+}
