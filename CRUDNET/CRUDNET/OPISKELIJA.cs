7f328f320c347d143500bde86ea032ae3bfba33d
Kaur-code <79134890+Kaur-code@users.noreply.github.com>
2021-11-16 20:30:59 +0200

    Add files via upload

---
+++ CRUDNET/CRUDNET/OPISKELIJA.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace CRUDNET
+{
+    class OPISKELIJA
+    {
+        Yhdista yhteys = new Yhdista();
+
+        public bool lisaaOpiskelija(string enimi, string snimi, string puh, string email, int onro)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            string lisakysely = "INSERT INTO yhteystiedot " +
+                "(etunimi, sukunimi, puhelin, sahkoposti, opiskelijanumero) " +
+                "VALUES (@enm, @snm, @puh, @eml, @ono); ";
+            komento.CommandText = lisakysely;
+            komento.Connection = yhteys.otaYhteys();
+
+            komento.Parameters.Add("@enm", MySqlDbType.VarChar).Value = enimi;
+            komento.Parameters.Add("@snm", MySqlDbType.VarChar).Value = snimi;
+            komento.Parameters.Add("@puh", MySqlDbType.VarChar).Value = puh;
+            komento.Parameters.Add("@eml", MySqlDbType.VarChar).Value = email;
+            komento.Parameters.Add("@ono", MySqlDbType.VarChar).Value = onro;
+
+            yhteys.avaaYhteys();
+            if(komento.ExecuteNonQuery() == 1)
+            {
+                yhteys.suljeYhteys();
+                return true;
+            }
+            else
+            {
+                yhteys.suljeYhteys();
+                return false;
+            }
+        }
+
+        public DataTable haeOpiskelijat()
+        {
+            MySqlCommand komento = new MySqlCommand("SELECT oid, etunimi, sukunimi, puhelin, sahkoposti, opiskelijanumero FROM yhteystiedot", yhteys.otaYhteys());
+            MySqlDataAdapter adapteri = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu);
+
+            return taulu;
+        }
+    }
+}
